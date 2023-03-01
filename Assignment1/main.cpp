9bd584b164c126e14fc5915b8f98bd15ca4d0c31
Barry King <barrywkingjr@gmail.com>
2023-03-01 15:32:01 -0600

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Barry on 3/1/23.
+//
+
+#include <iostream>
+
+using namespace std;
+const int COL_SIZE;
+void getData(int data[], );
+
+int main() {
+    const int ROW_SIZE;
+    
+
+    return 0;
+}
